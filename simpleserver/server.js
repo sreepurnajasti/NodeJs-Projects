8458da6dc18882fd395607e1f38d2dc3a8db7c75
sreepurnajasti <sreepurna.jasti@gmail.com>
2018-08-01 12:25:44 +0530

    serving html pages with http module

--- simpleserver/server.js
+++ simpleserver/server.js
@@ -1,14 +1,48 @@
-const http = require('http');
+const http = require('http')
+const url = require('url')
+const path = require('path')
+const fs = require('fs')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const mimeTypes =  {
+  'html': 'text/html',
+  'jpeg': 'image/jpeg',
+  'jpg': 'image/jpg',
+  'png': 'image/png',
+  'js': 'test/javascript',
+  'css': 'text/css'
+}
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+http.createServer(function(req, res){
+  var uri = url.parse(req.url).pathname
+  var fileName = path.join(process.cwd(), unescape(uri))
+  console.log('filename:'+ fileName);
+  var stats;
+  try{
+    stats = fs.lstatSync(fileName)
+    //console.log(stats)
+    if(stats.isFile()){
+      console.log('file')
+      // console.log(mimeType[path.extname(fileName).split('.').reverse()[0]])
+      var mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]]
+      res.writeHead(200, {'Content-type': mimeType})
+      var fileStream = fs.createReadStream(fileName)
+      fileStream.pipe(res)
+    } else if(stats.isDirectory()){
+      console.log('directory')
+      res.writeHead(302, {
+        'Location': 'page.html'
+      })
+      res.end()
+    } else{
+      res.writeHead(500, {'Content-type': 'text/plain'})
+      res.write('500 Internal Error\n')
+      res.end()
+    }
+  } catch(e){
+    console.log(e)
+    res.writeHead(404, {'content-type': 'text/plain'})
+    res.write('404 Not Found \n')
+    res.end()
+    return
+  }
+}).listen(1337)
